337db9d80c18f09122def3eba6e1212b0366cf69
avadavadagi <avadavadagi@avaya.com>
2022-05-02 23:10:28 +0100

    Initial repsoitory for link and comment

---
+++ src/main/java/com/achu/reddit_clone/repository/LinkRepository.java
@@ -0,0 +1,7 @@
+package com.achu.reddit_clone.repository;
+
+import com.achu.reddit_clone.model.Link;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LinkRepository extends JpaRepository<Link, Long> {
+}
